6e5d37f7388d06262752d6660e0bba504c181fdb
AhmedSamy16 <ahmed.samy1640@gmail.com>
2024-02-20 03:03:43 +0200

    Project completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import JSONDATA from "../assets/MOCK_DATA.json"
+
+type User = {
+  first_name: string; 
+  last_name: string; 
+  gender: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,14 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'search-filter';
+  private data =  JSONDATA
+  query = ''
+  filteredUsers: User[] = []
+  ngOnInit() {
+    this.filteredUsers = this.data
+  }
+  onChange(e: any) {
+    this.query = e.target.value
+    this.filteredUsers = this.data.filter(u => u.first_name.toLowerCase().includes(this.query.toLowerCase()))
+  }
 }
